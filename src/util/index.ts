fc3367d22ae139a958e7d6377d68755ddd2f549f
DC_Langx <dc_langx@hotmail.com>
2021-12-02 17:27:58 +0800

    节流改为防抖

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,13 +1,17 @@
-//节流
-export const throttle = (fn: () => void, time: number): (() => void) => {
-  let timer: null | number = null;
-  return () => {
-    if (timer) {
-      return false;
-    }
-    timer = setTimeout(() => {
-      fn();
-      timer = null;
-    }, time);
+// 防抖
+interface DebounceOrThrottle {
+  (bindFn: (...args: any[]) => void, wait?: number): (...args: any[]) => void;
+}
+export const debounce: DebounceOrThrottle = (fn, wait = 200) => {
+  let timer: number | undefined;
+
+  return (...args) => {
+    // 重置计时器
+    if (timer) clearTimeout(timer);
+
+    // 新计时器
+    timer = window.setTimeout(() => {
+      fn.apply(this, args);
+    }, wait);
   };
 };
